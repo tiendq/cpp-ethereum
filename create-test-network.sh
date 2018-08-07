de0cac035a1f1c9d30dff46017a24e804d3c49fe
Tien Do <tiendq@gmail.com>
2018-08-07 21:35:38 +0700

    Configure private test net

---
+++ create-test-network.sh
@@ -0,0 +1,1 @@
+./build/aleth/aleth --network-id 1999 --nodiscover --maxpeers 0 --datadir "/Users/tiendq/EthereumTestNet" --rpcport "8080" --rpccorsdomain "*" --port "30303" --identity "TienEthTestNode" init /Users/tiendq/GitHub/cpp-ethereum/genesis.json
